25b575d8d8a096daf58ddd5d1a7fdf76851b04f0
Christopher Rao <christopher.rao@points.com>
2019-08-11 13:40:06 -0400

    tests act as git user through context manager

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 import os
 import random
 import string
@@ -10,15 +11,27 @@ import pytest
 REPOS_DIR = '/content/repos'
 
 
-def random_string(string_length=10):
+def _random_string(string_length=10):
   """Generate a random string of fixed length """
   letters = string.ascii_lowercase
   return "".join(random.choice(letters) for i in range(string_length))
 
 
+@contextmanager
+def _as_git_user(user):
+  init_ssh_command = os.environ.get('GIT_SSH_COMMAND')
+  try:
+    os.environ['GIT_SSH_COMMAND'] = f'ssh -i /keys/{user}'
+    yield
+  finally:
+    if init_ssh_command is None:
+      del os.environ['GIT_SSH_COMMAND']
+    else:
+      os.environ['GIT_SSH_COMMAND'] = init_ssh_command
+
 @pytest.fixture
 def repo_name():
-  name = random_string()
+  name = _random_string()
   
   client = paramiko.SSHClient()
   client.load_host_keys(os.path.expanduser('~/.ssh/known_hosts'))
@@ -28,7 +41,8 @@ def repo_name():
 
   for user in ['test1', 'test2']:
     user_repo_path = os.path.join(REPOS_DIR, f'{user}_{name}')
-    git.Repo.clone_from(f'ssh://{user}@git/git/data/users/test1/{name}.git', user_repo_path, env={"GIT_SSH_COMMAND": f'ssh -i /keys/{user}'})
+    with _as_git_user(user):
+      git.Repo.clone_from(f'ssh://{user}@git/git/data/users/test1/{name}.git', user_repo_path)
 
   return name
 
